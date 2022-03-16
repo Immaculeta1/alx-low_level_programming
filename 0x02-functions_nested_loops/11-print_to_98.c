effb9509aedf2792fbbab59a50fcdd2c12b68105
NWOKEJEME IMMACULETA <immaculetapascaline@gmail.com>
2022-03-15 18:40:57 -0700

    task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,21 +13,21 @@ include <stdio.h>
 void print_to_98(int n)
 {
 
-  if (n < 98)
-    {
-      while (n < 98)
-	{
-	  printf("%d, ", n);
-	  n++;
-	}
-    }
-  else if (n > 98)
-    {
-      while (n > 98)
-	{
-	  printf("%d, ", n);
-	  n--;
-	}
-    }
-  printf("98\n");
+if (n < 98)
+{
+while (n < 98)
+{
+printf("%d, ", n);
+n++;
+}
+}
+else if (n > 98)
+{
+while (n > 98)
+{
+printf("%d, ", n);
+n--;
+}
+}
+printf("98\n");
 }
